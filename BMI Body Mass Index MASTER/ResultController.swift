37f33a1c068b7458fb82a854cebfedf52be54475
janusmols <janusbm@gmail.com>
2015-06-21 12:55:59 +0200

    s

---
+++ BMI Body Mass Index MASTER/ResultController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultController.swift
+//  
+//
+//  Created by Janus Broch Mols on 21/06/15.
+//
+//
+
+import UIKit
+
+class ResultController: UIViewController {
+
+    @IBOutlet weak var usersBMILabel: UILabel!
+    
+    @IBOutlet weak var usersBMIStatusLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
